7ec56c52d8161bc0c7fbb4d840afa73d1fa0dd8c
Anton Karpov <dulkahakana@gmail.com>
2022-03-28 21:37:23 +0300

    Открыть пост через переадресацию useNavigate

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import MyButton from './UI/button/MyButton'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const PostItem = ({number, post, remove}) => {
-    // const router = useHistory()
-    // console.log(router)
+    const {id, title, body} = post
+    
+    const navigate = useNavigate()
+    const goPost = () => navigate(`/posts/${id}`)
+
     return (
         <div className='post_box'> 
             <div className='post__content'>
-                <div className='post__title'>{post.id}. {post.title}</div>
+                <div className='post__title'>{id}. {title}</div>
                 <div className='text'>
-                    {post.body}
+                    {body}
                 </div>
             </div>
             <div className='post__btns'>
-                <MyButton
-                    className='open-post__btn'
-                    onClick={(f) => f}
-                >
-                    <Link to={`/posts/${post.id}`}>Открыть</Link>
-                </MyButton>
+                <MyButton onClick={goPost}>Открыть</MyButton>
                 <MyButton onClick={() => remove(post)}>Удалить</MyButton>
             </div>
         </div>
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 const PostIdPage = () => {
     const param = useParams()
+    
     return (
         <div className='block__title'>
             Вы открыли страницу поста с ID: {param.id}
